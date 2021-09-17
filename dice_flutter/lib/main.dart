3e0c7d5e6b320acb01f6ea6226abc45de108e6fb
Zhamshid Irisbayev <180103263@stu.sdu.edu.kz>
2021-09-17 06:38:55 +0600

    new changes

--- dice_flutter/lib/main.dart
+++ dice_flutter/lib/main.dart
@@ -1,29 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold
-      (
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        title: Text(
-          'Dice App',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
         backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text(
+            'Dice App',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Artifakt',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
       ),
-      body: DicePage(),
     ),
-  ),);
+  );
 }
-
-
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {},
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {},
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
